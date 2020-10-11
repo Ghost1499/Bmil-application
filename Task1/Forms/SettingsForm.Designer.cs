0e57eaa9c5f5758ae8e38c5bb91c2d3a2c73f8ac
Ghost1499 <zgstv@yandex.ru>
2020-10-11 17:08:08 +0300

    add settingsForm

---
+++ Task1/Forms/SettingsForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Task1.Forms
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.passwordSampleTextBox = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.acceptPasswordSampleButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(23, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(456, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Пароль для проведения тестов";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(23, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(248, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Алфавит пароля";
+            // 
+            // passwordSampleTextBox
+            // 
+            this.passwordSampleTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordSampleTextBox.Location = new System.Drawing.Point(30, 102);
+            this.passwordSampleTextBox.Name = "passwordSampleTextBox";
+            this.passwordSampleTextBox.Size = new System.Drawing.Size(222, 44);
+            this.passwordSampleTextBox.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(30, 260);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 3;
+            // 
+            // acceptPasswordSampleButton
+            // 
+            this.acceptPasswordSampleButton.Location = new System.Drawing.Point(330, 102);
+            this.acceptPasswordSampleButton.Name = "acceptPasswordSampleButton";
+            this.acceptPasswordSampleButton.Size = new System.Drawing.Size(86, 44);
+            this.acceptPasswordSampleButton.TabIndex = 4;
+            this.acceptPasswordSampleButton.Text = "Принять";
+            this.acceptPasswordSampleButton.UseVisualStyleBackColor = true;
+            this.acceptPasswordSampleButton.Click += new System.EventHandler(this.acceptPasswordSampleButton_Click);
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 450);
+            this.Controls.Add(this.acceptPasswordSampleButton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.passwordSampleTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "SettingsForm";
+            this.Text = "SettingsForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox passwordSampleTextBox;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button acceptPasswordSampleButton;
+    }
+}
---
+++ Task1/Forms/SettingsForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task1.Forms
+{
+    public partial class SettingsForm : Form
+    {
+        MainForm mainForm;
+        Settings settings;
+        public SettingsForm(MainForm mainForm, Settings settings)
+        {
+            InitializeComponent();
+            this.mainForm = mainForm;
+            this.settings = settings;
+            Init();
+        }
+
+        private void Init()
+        {
+            passwordSampleTextBox.Text = settings.Password;
+            System.Array alphabets = Enum.GetValues(typeof(PasswordManager.PasswordsAlphabets));
+            listBox1.DataSource = alphabets;
+            listBox1.DisplayMember = "Name";
+            //listBox1.ValueMember = "Value";
+
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+        }
+
+        void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // получаем id выделенного объекта
+            int value = (int)listBox1.SelectedValue;
+
+            // получаем весь выделенный объект
+            PasswordManager.PasswordsAlphabets passwordsAlphabet = (PasswordManager.PasswordsAlphabets)listBox1.SelectedItem;
+            //MessageBox.Show(value.ToString() + ". " + passwordsAlphabet.ToString());
+            settings.Alphabet = (PasswordManager.PasswordsAlphabets)value;
+            mainForm.UpdateLabels();
+
+        }
+
+        //private void passwordSampleTextBox_TextChanged(object sender, EventArgs e)
+        //{
+        //    settings.Password = passwordSampleTextBox.Text;
+        //    mainForm.UpdateLabels();
+        //}
+
+        private void acceptPasswordSampleButton_Click(object sender, EventArgs e)
+        {
+            settings.Password = passwordSampleTextBox.Text;
+            mainForm.UpdateLabels();
+        }
+    }
+    class Phone
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+
+}
+
