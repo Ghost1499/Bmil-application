2fbc0f40949142b52cafe80d40fded4c405afc50
Ghost1499 <zgstv@yandex.ru>
2020-11-28 22:35:57 +0300

    Добавлен отчет по 2 лабораторной

--- Task1/Math/HaarFunction.cs
+++ Task1/Math/HaarFunction.cs
@@ -8,23 +8,23 @@ namespace Task1
 {
     public static class HaarFunction
     {
-        public static Func<double,double>[] HaarVector(int N/*, double t*/)
+        public static Func<double, double>[] HaarVector(int N/*, double t*/)
         {
             if (N < 0)
                 throw new Exception("Wrong parametr N given");
             //double[] haarVector = new double[N];
-            Func<double,double>[] haarVector = new Func<double, double>[N];
-            haarVector[0] = (double t)=> 1;
+            Func<double, double>[] haarVector = new Func<double, double>[N];
+            haarVector[0] = (double t) => 1;
 
             int log = Convert.ToInt32(Math.Floor(Math.Log(N, 2)));
             int pow = 1;
-            for (int r = 0,count=1; r < log; r++)
+            for (int r = 0, count = 1; r < log; r++)
             {
-                for(int m = 1; m <= pow; m++,count++)
+                for (int m = 1; m <= pow; m++, count++)
                 {
                     int local_pow = pow;
                     int local_m = m;
-                    haarVector[count] =(double t)=> haar(local_pow, local_m, t);
+                    haarVector[count] = (double t) => haar(local_pow, local_m, t);
                 }
                 pow *= 2;
             }
@@ -38,11 +38,11 @@ namespace Task1
             if (t < 0 || t >= 1)
                 throw new Exception("Wrong time t parametr");
             double temp = ((double)m - 0.5) / pow;
-            if (t>=((double)(m-1))/pow && t < temp)
+            if (t >= ((double)(m - 1)) / pow && t < temp)
             {
                 return Math.Sqrt(pow);
             }
-            else if(t>= temp && t< (double)m / pow)
+            else if (t >= temp && t < (double)m / pow)
             {
                 return -Math.Sqrt(pow);
             }
