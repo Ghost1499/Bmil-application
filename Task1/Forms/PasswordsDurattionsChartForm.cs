85c7180ae485005c8e2d5ec43583377810e9b1d7
Ghost1499 <zgstv@yandex.ru>
2020-12-22 15:18:38 +0300

    Улучшена структура ChartForm

--- Task1/Forms/PasswordsDurattionsChartForm.cs
+++ Task1/Forms/PasswordsDurattionsChartForm.cs
@@ -27,12 +27,20 @@ namespace Task1.Forms
         protected DurationsStatistics EveningtimeStatistics { get; set; }
         private int CurrentUserId { get { return mainForm.AuthenticationController.CurrentUserId; } }
 
+        private Series DaytimePasswordsSeries { get; set; }
+        private Series EveningtimePasswordsSeries { get; set; }
+
         public PasswordsDurattionsChartForm(MainForm mainForm):base(mainForm)
         {
             InitializeComponent();
             
         }
         ~PasswordsDurattionsChartForm()
+        {
+            disposeContexts();
+        }
+
+        private void disposeContexts()
         {
             daytimePasswords.Dispose();
             eveningtimePasswords.Dispose();
@@ -44,22 +52,52 @@ namespace Task1.Forms
             morningTime = new TimeSpan(6, 0, 0);
             daytimePasswords = new PasswordActionContext();
             eveningtimePasswords = new PasswordActionContext();
-
             LoadData();
+
             DaytimeStatistics = new DurationsStatistics(daytimePasswords.PasswordActions.Local);
             EveningtimeStatistics = new DurationsStatistics(eveningtimePasswords.PasswordActions.Local);
             base.Init();
+        }
+        protected override void LoadData()
+        {
+            daytimePasswords.PasswordActions
+                .Include(p => p.SymbolActions)
+                .Where(p => p.UserId == CurrentUserId)
+                .Where(p => p.ValidPassword == Settings.Password)
+                .Where(p => DbFunctions.CreateTime(p.StartTime.Hour, p.StartTime.Minute, p.StartTime.Second)
+                < DbFunctions.CreateTime(eveninigTime.Value.Hours, eveninigTime.Value.Minutes, eveninigTime.Value.Seconds)
+                && DbFunctions.CreateTime(p.StartTime.Hour, p.StartTime.Minute, p.StartTime.Second)
+                >= DbFunctions.CreateTime(morningTime.Value.Hours, morningTime.Value.Minutes, morningTime.Value.Seconds))
+                .Load();
+
+            eveningtimePasswords.PasswordActions
+                .Include(p => p.SymbolActions)
+                .Where(p => p.UserId == CurrentUserId)
+                .Where(p => p.ValidPassword == Settings.Password)
+                .Where(p => DbFunctions.CreateTime(p.StartTime.Hour, p.StartTime.Minute, p.StartTime.Second)
+                >= DbFunctions.CreateTime(eveninigTime.Value.Hours, eveninigTime.Value.Minutes, eveninigTime.Value.Seconds)
+                || DbFunctions.CreateTime(p.StartTime.Hour, p.StartTime.Minute, p.StartTime.Second)
+                < DbFunctions.CreateTime(morningTime.Value.Hours, morningTime.Value.Minutes, morningTime.Value.Seconds))
+                .Load();
+            //MessageBox.Show(daytimePasswords.PasswordActions.Local.Count.ToString() + " " + eveningtimePasswords.PasswordActions.Local.Count.ToString(), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
         }
         protected override void InitChart()
         {
             base.InitChart();
             //SeriesCollection series = Series;
             chart.Titles[0].Text = "Длительность набора парольной фразы в секундах";
-            chart.Series.Last().Points.DataBindY(daytimePasswords.PasswordActions.Local, "TimeDuration");
+            //chart.Series.Last().Points.DataBindY(daytimePasswords.PasswordActions.Local, "TimeDuration");
             chart.Series.Last().ChartType = SeriesChartType.Column;
+            DaytimePasswordsSeries = chart.Series.Last();
+
             chart.Series.Add(new Series("Вечером"));
             chart.Series.Last().ChartType = SeriesChartType.Column;
-            chart.Series.Last().Points.DataBindY(eveningtimePasswords.PasswordActions.Local,"TimeDuration");
+            //chart.Series.Last().Points.DataBindY(eveningtimePasswords.PasswordActions.Local,"TimeDuration");
+            EveningtimePasswordsSeries = chart.Series.Last();
+
+            //UpdateChart();
+
         }
         protected override void InitLabelsGroupBox()
         {
@@ -88,46 +126,27 @@ namespace Task1.Forms
             //LabelControllers.Add(new LabelController("Дисперсия в миллискундах", Statistics.GetPasswordsDispersion, layoutPanel));
             //LabelControllers.Add(new LabelController("Среднеквадратическое отклонение", Statistics.GetPasswordsSigma, layoutPanel));
         }
-        private void LoadData()
-        {
-            daytimePasswords.PasswordActions
-                .Include(p => p.SymbolActions)
-                .Where(p => p.UserId == CurrentUserId)
-                .Where(p => p.ValidPassword == Settings.Password)
-                .Where(p => DbFunctions.CreateTime(p.StartTime.Hour, p.StartTime.Minute, p.StartTime.Second)
-                < DbFunctions.CreateTime(eveninigTime.Value.Hours, eveninigTime.Value.Minutes, eveninigTime.Value.Seconds)
-                && DbFunctions.CreateTime(p.StartTime.Hour, p.StartTime.Minute, p.StartTime.Second)
-                >= DbFunctions.CreateTime(morningTime.Value.Hours, morningTime.Value.Minutes, morningTime.Value.Seconds))
-                .Load();
-
-            eveningtimePasswords.PasswordActions
-                .Include(p => p.SymbolActions)
-                .Where(p => p.UserId == CurrentUserId)
-                .Where(p => p.ValidPassword == Settings.Password)
-                .Where(p => DbFunctions.CreateTime(p.StartTime.Hour, p.StartTime.Minute, p.StartTime.Second)
-                >= DbFunctions.CreateTime(eveninigTime.Value.Hours, eveninigTime.Value.Minutes, eveninigTime.Value.Seconds)
-                || DbFunctions.CreateTime(p.StartTime.Hour, p.StartTime.Minute, p.StartTime.Second)
-                < DbFunctions.CreateTime(morningTime.Value.Hours, morningTime.Value.Minutes, morningTime.Value.Seconds))
-                .Load();
-            //MessageBox.Show(daytimePasswords.PasswordActions.Local.Count.ToString() + " " + eveningtimePasswords.PasswordActions.Local.Count.ToString(), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            
-        }
-        protected override void UpdateForm(/*object sender=null, NotifyCollectionChangedEventArgs e=null*/)
-        {
-            LoadData();
-            chart.Series[0].Points.DataBindY(daytimePasswords.PasswordActions.Local, "TimeDuration");
-            chart.Series[1].Points.DataBindY(eveningtimePasswords.PasswordActions.Local, "TimeDuration");
-            base.UpdateForm(/*sender, e*/);
-        }
-        //protected override void UpdateChart()
+        
+        //protected override void UpdateForm()
         //{
-
-
-        //    double[] yValues = Statistics.GetPasswordsDurations();
-        //    UpdateChartByData(yValues);
-
+        //    LoadData();
+        //    chart.Series[0].Points.DataBindY(daytimePasswords.PasswordActions.Local, "TimeDuration");
+        //    chart.Series[1].Points.DataBindY(eveningtimePasswords.PasswordActions.Local, "TimeDuration");
+        //    base.UpdateForm();
         //}
-        public override void Clear()
+
+
+        protected override void UpdateChart()
+        {
+            DaytimePasswordsSeries.Points.DataBindY(daytimePasswords.PasswordActions.Local, "TimeDuration");
+            EveningtimePasswordsSeries.Points.DataBindY(eveningtimePasswords.PasswordActions.Local, "TimeDuration");
+
+        }
+        
+        
+
+
+        protected override void ReloadData()
         {
             daytimePasswords = new PasswordActionContext();
             eveningtimePasswords = new PasswordActionContext();
@@ -136,12 +155,9 @@ namespace Task1.Forms
             DaytimeStatistics.ChangeCollection(daytimePasswords.PasswordActions.Local);
             EveningtimeStatistics.ChangeCollection(eveningtimePasswords.PasswordActions.Local);
 
-            chart.Series[0].Points.DataBindY(daytimePasswords.PasswordActions.Local, "TimeDuration");
-            chart.Series[1].Points.DataBindY(eveningtimePasswords.PasswordActions.Local, "TimeDuration");
-
-            UpdateForm();
-            //daytimePasswords.PasswordActions.Local.Clear();
-            //eveningtimePasswords.PasswordActions.Local.Clear();
+            UpdateChart();
+            UpdateLablesGroupBox();
         }
+
     }
 }
